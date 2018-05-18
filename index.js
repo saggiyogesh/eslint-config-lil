df32d36c4641b45d97fd6916d299b9bf7e2caa2f
saggiyogesh <saggiyogesh@gmail.com>
2018-05-18 13:26:22 +0530

    impl

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['xo-space/esnext', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'capitalized-comments': 'off',
+    'object-curly-spacing': 'off',
+    'new-cap': 'warn',
+    camelcase: 'warn',
+    radix: 'off',
+    'no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true
+      }
+    ],
+    'no-await-in-loop': 'warn',
+    'object-shorthand': 'error',
+    'array-callback-return': 'error',
+    'prefer-destructuring': 'error'
+  }
+};
